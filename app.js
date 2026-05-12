a4928c04eb957d401de7eec9175bdc59ada33f87
liuwenying <liuwenying>
2026-05-13 01:07:38 +0800

    字体优化

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ App({
   onLaunch() {
     usageAnalytics.onAppLaunch()
     this._initTheme()
+    this._loadDisplayFont()
   },
 
   onShow() {
@@ -28,8 +29,20 @@ App({
     }
   },
 
+  _loadDisplayFont() {
+    wx.loadFontFace({
+      global: true,
+      family: 'DaoDisplay',
+      source: 'url("https://cdn.jsdelivr.net/npm/lxgw-wenkai-webfont@1.7.0/fonts/LXGWWenKai-Regular.woff2")',
+      scopes: ['webview', 'native'],
+      success: () => { this.globalData.fontLoaded = true },
+      fail: () => { this.globalData.fontLoaded = false }
+    })
+  },
+
   globalData: {
     userProfile: null,
-    systemTheme: 'light'
+    systemTheme: 'light',
+    fontLoaded: false
   }
 })
